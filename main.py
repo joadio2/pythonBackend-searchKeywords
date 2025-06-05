f134814936a9bc70a13fddaeed3333b7dc3e23cb
joadio2 <joaquin.dionicioo@gmail.com>
2025-06-05 14:40:58 +0100

    health

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import asyncio
 from fastapi import FastAPI
 import httpx
 from routes import scoreRoute
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(scoreRoute.score)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/health")
+@app.get("/")
 async def health_check():
     return 200
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
